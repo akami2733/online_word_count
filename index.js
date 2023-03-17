ddefb5ad5e9828fa5b2ff3bafb40607ed15199cd
akami <akami2733@gmail.com>
2023-03-17 18:27:33 +0800

    [bug report] incorrect word count for japanese #1

--- index.js
+++ index.js
@@ -9,16 +9,23 @@ function update_stat(evt) {
 }
 
 function count_words(text) {
-    let new_word = true
     let word_count = 0
-    for (const c of text) {
-        if (c === " ")
-            new_word = true
-        else {
-            if (new_word)
-                word_count++
-            new_word = false
-        }
+    let is_previous_alphabet_number = false
+    let is_space
+    let is_alphabet_number
+    let char_code
+    
+    for (let i = 0; i < text.length; i++) {
+        char_code = text.charCodeAt(i)
+        is_space = char_code === 32
+        is_alphabet_number = (char_code >= 48 && char_code <= 57) || 
+            (char_code >= 65 && char_code <= 90) || 
+            (char_code >= 97 && char_code <= 122)
+        if (is_alphabet_number && is_previous_alphabet_number)
+            continue
+        is_previous_alphabet_number = is_alphabet_number
+        if (!is_space)
+            word_count++
     }
     return word_count
 }
